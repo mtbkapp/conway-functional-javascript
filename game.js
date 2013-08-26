d2c34b7f463c34a382f428649f433f6e10196a7d
Jason Kapp <mtbkapp@gmail.com>
2013-08-26 10:14:10 -0600

    My first solution
    My first solution based on one from the book Clojure Programming

--- game.js
+++ game.js
@@ -1,49 +1,83 @@
+// implmentation adapted from Clojure Programming by Emerick, Grande, Craper
+// Clojure source at: https://github.com/clojurebook/ClojureProgramming/blob/master/ch03-game-of-life/src/com/clojurebook/collections/life.clj
+
 function makeGame() {
-    /*
-     * Conway's Game of Life
-     * ----------------------------- 
-     *  Any live cell with fewer than two live neighbours dies, as if caused by under-population.
-     *  Any live cell with two or three live neighbours lives on to the next generation.
-     *  Any live cell with more than three live neighbours dies, as if by overcrowding.
-     *  Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-     *  
-     */
+    var width = 75;
+    var height = 40;
 
-    /*
-     * Other rules specific to this activity are on the html page
-     */
-
-    /*
-     * What to implement
-     * ---------------------------------
-     * The getGame function is to return an object that configures the game.
-     * The keys the object should contain are: 
-     *      width: - the width of the world as a number of cells, defaults to: 75 
-     *      height: - the height of the world as a number of cells, defaults to: 40,
-     *      init: - the initial state of the board, defaults to: [],
-     *      transform - a function that takes the current state of the world and transforms it
-     *                  to the next state, defaults to return random states.
-     *                  This function is where Conway's rules should be implemented 
-     *      wait - how long to wait, in ms, before in between calls to transform, default 250
-     *
-     * The "schema" for the state of the board.  An array of the of arrays where the inner array
-     * is the x, y position of a live cell.  All other cells will be assumed to be dead.  The origin
-     * of the world is at the upper left corner.  The x is the number of cells to the left.  The y
-     * is the number of cells down.  The origin is at [0,0].
-     *
-     * Example:
-     * [[23,33],[22,33],[22,34]] 
-     * cells at [23,33], [22,33], [22,34] are alive.  all others are dead.
-     *
-     *
-     */
+    var onEarth = function(cell) {
+        return cell[0] >= 0 &&
+               cell[0] < width &&
+               cell[1] >= 0 &&
+               cell[1] < height;
+    };
 
 
-    /*
-     * A good test to see if you implemented it correctly would be to make a glider for the
-     * initial state.
-     */
+    var crossProduct = function(x, y) {
+        return _.flatten(_.map(y, function(yi) {
+            return _.map(x, function(xi) {
+                return [xi, yi];
+            });
+        }), true);
+    };
+
+    var addCell = function(cellX, cellY) {
+        return [cellX[0] + cellY[0], cellX[1] + cellY[1]];
+    };
+
+    var neighbors = function(cell) {
+        var d = [-1, 0, 1];
+        return _.chain(crossProduct(d,d))
+                    .reduce(function(neighbors, delta) {
+                        if (delta[0] == 0 && delta[1] == 0) {
+                            return neighbors;
+                        }
+                        
+                        return neighbors.concat([addCell(delta, cell)]);
+                    }, [])
+                    .filter(onEarth)
+                    .value();
+    };
+
+    var frequencyReducer = function(freqs, cell) {
+        var id = cell.toString();
+        if (_.has(freqs, id)) {
+            freqs[id].n++;
+        } else {
+            freqs[id] = { cell: cell, n: 1 };
+        }
+
+        return freqs;
+    };
+
+    var isSameCell = function(cellX, cellY) {
+        return cellX[0] == cellY[0] &&
+               cellX[1] == cellY[1];
+    };
+
+    var step = function(state) {
+        return _.chain(state)
+                .map(neighbors)  //get all the neighbor cells, there may be multiples of each cell
+                .flatten(true)
+                .reduce(frequencyReducer, {})  // count how many duplicate cells there are 
+                .filter(function(f) {
+                    //base on the count I got in the previous step
+                    //get rid of the cells that shouldn't live
+                    return ((_.some(state, partial(isSameCell, f.cell))) ?
+                                (f.n == 2 || f.n == 3) : 
+                                (f.n == 3));
+                })
+                .map(function(f) {
+                    return f.cell
+                })
+                .value();
+    };
+
     return {
-
+        height: height,
+        width: width,
+        wait: 100,
+        init: [[1,3], [2,3], [3,3], [3,2], [2,1]], //it's a glider
+        transform: step 
     };
 };
